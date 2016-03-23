1d19533adc2df79093757b114c38df2cd719fa6d
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-03-23 02:06:37 +0000

    Playin around with traits.

--- misc/generic_traits.rs
+++ misc/generic_traits.rs
@@ -1,29 +1,33 @@
 //use std::ops::{Add, Sub, Mul};
 
-trait Shape<T>
-   where T: Mul<Output=T> + Copy
-{
-   fn get_height(&self) -> T;
-   fn get_width(&self) -> T;
-   fn get_area(&self) -> T;
-   fn get_perimiter(&self) -> T;
+trait Shape {
+   fn get_height(&self) -> i32;
+   fn get_width(&self) -> i32;
+   fn get_area(&self) -> i32;
+   fn get_perimiter(&self) -> i32;
 }
 
-struct Square<T> {
-   height: T, width: T
+#[derive(Copy, Clone)]
+struct Rectangle {
+   height: i32, width: i32
 }
-struct Circle<T> {
-   radius: T
+//struct Circle { radius: i32 }
+
+impl Shape for Rectangle {
+   fn get_height(&self) -> i32 { self.height }
+   fn get_width(&self) -> i32 { self.width }
+   fn get_area(&self) -> i32 { self.height * self.width }
+   fn get_perimiter(&self) -> i32 { self.height+self.height + self.width+self.width }
 }
 
-impl<T> Shape<T> for Square<T> {
-   fn get_height(&self) -> T { self.height }
-   fn get_width(&self) -> T { self.width }
-   fn get_area(&self) -> T { self.height * self.width }
-   fn get_perimiter(&self) -> T { self.height+self.height + self.width+self.width }
-}
+fn test<T: Shape>(x: T) -> i32 { x.get_height() }
+
+fn test2(x: &Shape) -> i32 { x.get_height() } //x.get_area() }
 
 fn main() {
-   //let x = Square{ height: 10, width: 10 };
-   println!("{}", 3);//x.get_height());
+   let x = Rectangle { height: 10, width: 10 };
+   //println!("{}", x.get_height());
+   println!("{}", test(x));
+   println!("{}", test2(&x));
+
 }
