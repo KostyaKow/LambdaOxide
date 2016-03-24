6a4fd94ed26c4bff09374dcfadb700755e66e3e5
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-03-24 03:42:59 +0000

    Lifetime nightmares

--- lisp.rs
+++ lisp.rs
@@ -309,10 +309,14 @@ enum FunType {
    BuiltIn(Box<Fn(Cons<Sexps>) -> Sexps>),
    Lambda(Sexps)
 }
-struct Callable { env : SymTable, f : FunType, arg_names : Cons<String> }
-impl Callable {
-   fn new(arg_names : Cons<String>, f : FunType, parent_env : Box<SymTable>) -> Callable {
-      Callable { env: SymTable::new(Some(parent_env)), f: f, arg_names: arg_names }
+struct Callable<'a> { env : SymTable<'a>, f : FunType, arg_names : Cons<String> }
+impl<'a> Callable<'a> {
+   fn new(arg_names : Cons<String>, f : FunType, parent_env : Box<&'a SymTable<'a>>) -> Callable<'a> {
+      Callable {
+         env: SymTable::new(Some(parent_env)),
+         f: f,
+         arg_names: arg_names
+      }
    }
    fn exec(&self, args : Cons<Sexps>) -> Sexps {
       match self.f { _ => Sexps::Err("calling .exec of callable".to_string()), }
@@ -320,45 +324,47 @@ impl Callable {
 }
 
 //symtable
-struct SymTable {
-   bindings : HashMap<String, Callable>,
-   parent : Option<Box<SymTable>>
+struct SymTable<'a> {
+   bindings : HashMap<String, Callable<'a>>,
+   parent : Option<Box<&'a SymTable<'a>>>
 }
 
-fn add_defaults(env : Box<SymTable>) {
-   let sum_ = |args_ : Cons<Sexps>| -> Sexps  {
-      let mut args = Box::new(args_);
-      let mut sum = 0;
-      loop {
-         match *args {
-            Cons::Cons(Sexps::Num(n), y) => { sum += n; args = y; },
-            Cons::Nil   => break,
-            _ => return Sexps::Err("bad arguments to sum".to_string())
-         };
-      }
-      Sexps::Num(sum)
-   };
-   //let difference_ = | |
-
-   //unsafe {
-   let sum = Callable::new(Cons::Single("*".to_string()), //* = any arg
-                           FunType::BuiltIn(Box::new(sum_)),
-                           env);
-
-   //let difference = Callable::new(Cons::Single(
-   env.add("+".to_string(), sum)
-   //self.add("-".to_string(), difference)
-   //}
-}
-
-impl SymTable {
-   fn new(parent : Option<Box<SymTable>>) -> SymTable {
+impl<'a> SymTable<'a> {
+   fn new(parent : Option<Box<&'a SymTable>>) -> SymTable<'a> {
       SymTable {
          bindings : HashMap::new(),
          parent   : parent,
       }
    }
-   fn add(&mut self, key : String, f : Callable) { self.bindings.insert(key, f); }
+
+   fn add_defaults(&'a mut self) {
+      let sum_ = |args_ : Cons<Sexps>| -> Sexps  {
+         let mut args = Box::new(args_);
+         let mut sum = 0;
+         loop {
+            match *args {
+               Cons::Cons(Sexps::Num(n), y) => { sum += n; args = y; },
+               Cons::Nil   => break,
+               _ => return Sexps::Err("bad arguments to sum".to_string())
+            }
+         }
+         Sexps::Num(sum)
+      };
+
+      //let difference_ = | |
+      //unsafe {}
+      let sum = {
+         Callable::new(Cons::Single("*".to_string()), //* = any arg
+                       FunType::BuiltIn(Box::new(sum_)),
+                       Box::new(self))
+      };
+
+      //let difference = Callable::new(Cons::Single(
+      self.add("+".to_string(), sum)
+      //self.add("-".to_string(), difference)
+      //}
+   }
+   fn add(&mut self, key : String, f : Callable<'a>) { self.bindings.insert(key, f); }
    fn lookup(&self, s : &String) -> Option<&Callable> {
       //if !self.bindings.contains_key(s)
       let entry_opt = self.bindings.get(s);
@@ -440,7 +446,7 @@ fn run(code : &str) -> Sexps {
    let exp_opt = parse(&lexemes);
 
    let mut env = SymTable::new(None);
-   add_defaults(Box::new(env));
+   env.add_defaults();
 
    if let Some(exp) = exp_opt {
       eval(&exp, &mut env)
