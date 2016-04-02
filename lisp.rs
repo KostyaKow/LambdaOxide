0e90fc4ca9f5f9d9740aaaa2cc2c6db16f3966db
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-04-02 19:33:00 +0000

    Fixed problems with lambda's, now church numeral example works perfectly!

--- lisp.rs
+++ lisp.rs
@@ -43,6 +43,8 @@ impl Callable {
       match *self {
          Callable::BuiltIn(parent, ref f) => { f(args_exp, root, parent) },
          Callable::Lambda(ref t, ref arg_names_exp, ref exp) => {
+            let exec_table = root.borrow_mut().table_new(t.clone());
+
             debug_p(2, "Calling .exec() of lambda");
             if let Sexps::Sub(box ref args_@Cons::Cons(_, _)) = args_exp {
                if let Sexps::Sub(box ref arg_names_@Cons::Cons(Sexps::Var(_), _)) = *arg_names_exp {
@@ -52,7 +54,8 @@ impl Callable {
                   while let Cons::Cons(Sexps::Var(ref arg_name), box ref rest) = *arg_names {
                      if let Some(arg) = cons_get(&args_, i) {
                         let mut borrowed = root.borrow_mut();
-                        borrowed.table_add(t.clone(), &*arg_name, make_sym_table_val(arg.clone()));
+                        let val = make_sym_table_val(arg.clone());
+                        borrowed.table_add(exec_table.clone(), &*arg_name, val);
                         arg_names = &rest;
                         i += 1;
                      } else { return err("argument number doesn't match") }
@@ -76,7 +79,7 @@ impl Callable {
                //let mut i = 0;
                //for arg in args { env.add(arg_names[i], arg); i+=1; }
             } else { kkleft: env.add("*", args); } */
-            eval(exp, root, t.clone()) //kkleft: eval or apply
+            eval(exp, root, exec_table.clone()) //kkleft: eval or apply
          }
       }
    }
@@ -118,8 +121,11 @@ impl Env {
       env
    }
    fn print(&self) {
+      /*println!("printing table");*/
+      let mut i = 0;
       for table in &self.tables {
-         println!("table...parent: {}", table.parent);
+         println!("table {}...parent: {}", i, table.parent);
+         i+=1;
          for (ref key, ref binding) in &table.bindings {
             //println!("key: {}, binding: {}", key, binding);
             print_space(3); println!("key: {}", key);
@@ -163,6 +169,9 @@ impl Env {
    }
    fn add_defaults(&mut self) {
       let sum_ = |args_sexps : Sexps, root : Root, table : EnvId| -> Sexps {
+         if let Sexps::Err(ref s) = args_sexps {
+            debug_p(5, "variable lookup calling exec of diff"); return err(s);
+         }
          let mut sum = 0;
          if let Sexps::Sub(box ref args_) = args_sexps {
             let mut args : &Cons<Sexps> = args_; //Box::new(args_);
@@ -184,6 +193,9 @@ impl Env {
       self.table_add(0, "+", sum);
 
       let diff_ = |args_sexps : Sexps, root : Root, table : EnvId| -> Sexps {
+         if let Sexps::Err(ref s) = args_sexps {
+            debug_p(5, "variable lookup calling exec of diff"); return err(s);
+         }
          let mut diff = 0;
          let mut first = true;
 
@@ -210,6 +222,10 @@ impl Env {
       self.table_add(0, "-", diff);
 
       let load_ = |args_sexps : Sexps, root : Root, table : EnvId| -> Sexps {
+         if let Sexps::Err(ref s) = args_sexps {
+            debug_p(5, "variable lookup calling exec of load"); return err(s);
+         }
+
          //TODO: kkleft assert rest = Null
          use std::io::prelude::*;
          use std::fs::File;
@@ -248,6 +264,17 @@ impl Env {
       };
       let load = Callable::BuiltIn(0, Box::new(load_));
       self.table_add(0, "load", load);
+
+      let print_root_ = |args_sexps : Sexps, root : Root, table : EnvId| -> Sexps {
+         if let Sexps::Err(ref s) = args_sexps {
+            debug_p(5, "variable lookup calling exec of load"); return err(s);
+         }
+
+         root.borrow().print();
+         err("succ")
+      };
+      let print_root = Callable::BuiltIn(0, Box::new(print_root_));
+      self.table_add(0, "print_env", print_root);
    }
 }
 
@@ -267,6 +294,7 @@ fn eval(exp : &Sexps, root : Root, table : EnvId) -> Sexps {
       ref l@Sexps::Lambda(..)       => l.clone(),
       Sexps::Err(ref s)             => Sexps::Err(s.clone()),
       Sexps::Var(ref s)             => {
+         debug_p(2, "Looking up variable");
          let borrowed = root.borrow();
          let lookup_opt = borrowed.lookup(table, &s.clone());
          //if let Some(&Callable::BuiltIn(..)) = lookup_opt { Sexps::Var(s.clone()) }
@@ -304,15 +332,15 @@ fn apply_macro(name : &str, args : &Cons<Sexps>, root : Root, t : EnvId) -> Sexp
       "lambda" => {
          debug_p(3, "macro lambda");
          if let Cons::Cons(ref args@Sexps::Sub(_), box Cons::Cons(ref exp, _)) = *args {
-            let borrowed = unsafe { root.as_unsafe_cell().get() };
-
             if DEBUG >= 3 {
                print_space(3); print!("args: "); print_compact_tree(args);
                print_space(5); print!("exp: "); print_compact_tree(exp);
             }
-
-            let lambda = Callable::Lambda(t, args.clone(), exp.clone());
+            let borrowed = unsafe { root.as_unsafe_cell().get() };
             let lambda_table = unsafe { (*borrowed).table_new(t) };
+
+            println!("new lambda table number {}", lambda_table);
+            let lambda = Callable::Lambda(lambda_table, args.clone(), exp.clone());
             root.borrow_mut().table_add(lambda_table, "self", lambda);
             Sexps::Lambda(lambda_table, "self".to_string())
             //var params = kkzz
@@ -459,7 +487,7 @@ fn interpreter() {
       let line = stdin.lock().lines().next().unwrap().unwrap();
       let out = run(&root, &line);
       display_sexps(&out);
-      root.borrow().print();
+      /*root.borrow().print();*/
    }
 }
 
