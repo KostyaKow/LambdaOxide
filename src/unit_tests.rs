748f9510ca2f363e1da088c018786d9783c32e7b
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-04-05 02:35:09 +0000

    Improving unit testing infrastructure.

--- src/unit_tests.rs
+++ src/unit_tests.rs
@@ -4,25 +4,37 @@ use types::{Lexeme, print_lexemes};
 use lexer::lex;
 use err::PRINT_TESTS;
 
+type LexTestResult = (String, Vec<Lexeme>);
+
 #[test]
 fn test_lex() {
-   let mut s = "(+ 3 5 0.1 -5 -5.0";
-   //let mut s = "-5 -5.0";
+   check_lex_test(test_lex_1());
+}
 
-   let lexed = lex(s);
+fn check_lex_test(r : LexTestResult) {
+   let (input, expected) = r;
 
-   let mut good = Vec::new();
-   good.push(Lexeme::Sym("+".to_string()));
-   good.push(Lexeme::Int(3));
-   good.push(Lexeme::Float(0.1));
-   good.push(Lexeme::Int(-5));
-   good.push(Lexeme::Float(-5.0));
+   let lexed = lex(&input);
 
-   if !vec_eq(&good, &lexed) {
+   if !vec_eq(&expected, &lexed) {
       print_lexemes(&lexed);
-      panic!("bad lexemes test");
+      panic!("failed lexeme test 1");
    }
-   //assert!(vec_eq(good, lexed));
+}
+
+fn test_lex_1() -> LexTestResult {
+   let input = "(+ 3 5 0.1 -5 -5.0";
+   //let mut s = "-5.0";
+
+   let mut expected = Vec::new();
+   expected.push(Lexeme::Sym("+".to_string()));
+   expected.push(Lexeme::Int(3));
+   expected.push(Lexeme::Float(0.1));
+   expected.push(Lexeme::Int(-5));
+   expected.push(Lexeme::Float(-5.0));
+
+   return (input.to_string(), expected);
+   //assert!(vec_eq(expected, lexed));
    //assert_eq!(iter.next(),
    //panic!("fail");
 }
